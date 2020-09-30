9eb8af7cc5b60cf08aadbc20d85eb38d6c8c35b9
Alexander Ulyanov <alexander34396@mail.ru>
2020-09-30 16:46:16 +0300

    Added find by datetime and boundaries method

--- src/main/java/ru/spb/idu/incidents/repository/IncidentRepo.java
+++ src/main/java/ru/spb/idu/incidents/repository/IncidentRepo.java
@@ -1,16 +1,24 @@
 package ru.spb.idu.incidents.repository;
 
+import org.geolatte.geom.Geometry;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import ru.spb.idu.incidents.domain.Incident;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.List;
 
 @RepositoryRestResource
 public interface IncidentRepo extends PagingAndSortingRepository<Incident, Long> {
   @RestResource(path = "getByDate", rel = "getByDate")
-  List<Incident> findAllByStartGreaterThanEqualAndStartLessThanEqual(
-          Date minDate, Date maxDate);
+  List<Incident> findAllByStartGreaterThanEqualAndFinishLessThanEqual(Date minDate, Date maxDate);
+
+  @Query(
+      value =
+          "select i from Incident i where i.start >= ?1 and i.finish <= ?2 and intersects(i.geometry, ST_SetSRID(?3, 4326)) = true")
+  List<Incident> findAllByTimeAndGeometry(
+      Timestamp minStamp, Timestamp maxStamp, Geometry boundaries);
 }
